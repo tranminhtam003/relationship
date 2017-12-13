14d93e316413541f0b17b5fb8008d1b3e0d1a47b
tranminhtam003 <tmtam@tma.com.vn>
2017-12-13 10:01:07 +0700

    + updated code

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Seeder;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('users')->insert([
+        	'name' => str_random(10),
+        	'email' => str_random(10).'@gmail.comm',
+        	'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+        	'name' => str_random(10),
+        	'email' => str_random(10).'@gmail.comm',
+        	'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+        	'name' => str_random(10),
+        	'email' => str_random(10).'@gmail.comm',
+        	'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+        	'name' => str_random(10),
+        	'email' => str_random(10).'@gmail.comm',
+        	'password' => bcrypt('secret'),
+        ]);
+
+        DB::table('users')->insert([
+        	'name' => str_random(10),
+        	'email' => str_random(10).'@gmail.comm',
+        	'password' => bcrypt('secret'),
+        ]);
+    }
+}
